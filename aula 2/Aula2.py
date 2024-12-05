2b9c8dd9599011f4dc174debd28a66e1d522e30c
Ini-Bsantis <viniciusbarrosdossantos75@gmail.com>
2024-12-05 11:26:24 -0300

    aprendi e reforcei o print e variações

---
+++ aula 2/Aula2.py
@@ -0,0 +1,17 @@
+print('Carlos é legal')
+
+nome_usuario='VINICIUS'# TIPO -> STR - String
+
+idade_usuario=20 # tipo ->  int = Integer
+
+altura_usuario= 1.69 # tipo -> float = flutuante
+
+maior_idade= False # tipo -> bool = boolean
+
+print('Seja bem - vindo',nome_usuario)
+
+print('sua idade é',idade_usuario,"anos")
+
+print('você tem altura',altura_usuario,"metros")
+
+print('Você é maior de idadde',maior_idade)
